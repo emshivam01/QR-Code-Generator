3896199aaca3880acedb532b4e46a9355083723d
Shivam Sharma <emshivam01@gmail.com>
2022-11-07 18:32:20 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let input = document.getElementById("value");
+let btn = document.getElementById("btn");
+let qrCode = document.getElementById("qr-code");
+let section = document.getElementById("sec");
+
+btn.addEventListener("click", () => {
+  let value = input.value;
+  console.log(value);
+  if (!value) return;
+  qrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${value}`;
+  input.value = "";
+});
